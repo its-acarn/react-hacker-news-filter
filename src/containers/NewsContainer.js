31f65bce26940ba86391094fd42afe884b7cf71b
svvishjs <andreww92@live.com>
2020-12-09 12:04:04 +0000

    added Promise.all for each of the top story Id's. API state now working

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -2,25 +2,44 @@ import {useState, useEffect } from 'react';
 
 const NewsContainer = () => {
 
-  const [loaded, setLoaded] = useState(false);
+  const [apiLoaded, setApiLoaded] = useState(false);
+  const [promiseAllLoaded, setPromiseAllLoaded] = useState(false);
   const [topStoriesID, setTopStoriesID] = useState([]);
+  const [topStories, setTopStories] = useState([]);
 
   const topStoriesURL = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-  
+
   const getTopStoriesIDList = () => {
     fetch(topStoriesURL)
       .then(res => res.json())
       .then(data => setTopStoriesID(data))
-      .then(() => setLoaded(true))
-  }
+      .then(() => setApiLoaded(true))
+  };
+
+  const getStoryById = (id) => {
+    return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json `)
+  };
+
+  const getAllStories = () => {if (!apiLoaded){
+  Promise.all(topStoriesID.map(id => 
+    getStoryById(id)
+    .then(res => res.json())
+    .then(data => setTopStories([...topStories,data]))
+    .then(console.log("change"))
+  )).then(setPromiseAllLoaded(true))
+  }}
 
   useEffect(() => {
     getTopStoriesIDList();
-  },[])
+  },[]);
+
+  useEffect(() => {
+    getAllStories();
+  }, [topStoriesID])
 
   return (
     <main>
-      {setTopStoriesID}
+      
     </main>
 
   )
